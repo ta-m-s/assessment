deba535ed58ea020ce2b54f1218af80e38408549
ta-m-s <ta_mihiro_saito@nnn.ac.jp>
2026-02-14 17:23:17 +0900

    CSSフレームワークによるデザインの改良

--- assessment.js
+++ assessment.js
@@ -13,14 +13,24 @@ assessmentButton.addEventListener(
     }
 
     resultDivision.innerText = '';
-    const heading = document.createElement('h3');
-    heading.innerText = '診断結果';
-    resultDivision.appendChild(heading);
+    
+    const headerDivision = document.createElement('div');
+    headerDivision.setAttribute('class', 'card-header text-bg-primary');
+    headerDivision.innerText = '診断結果';
+
+    const bodyDivision = document.createElement('div');
+    bodyDivision.setAttribute('class', 'card-body');
 
     const paragraph = document.createElement('p');
+    paragraph.setAttribute('class', 'card-text');
     const result = assessment(userName);
     paragraph.innerText = result;
-    resultDivision.appendChild(paragraph);
+    bodyDivision.appendChild(paragraph);
+
+    resultDivision.setAttribute('class', 'card');
+
+    resultDivision.appendChild(headerDivision);
+    resultDivision.appendChild(bodyDivision);
 
     tweetDivision.innerText = '';
     const anchor = document.createElement('a');
